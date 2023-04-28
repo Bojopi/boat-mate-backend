af99984917a741bc07b8a86af4a29a7b4fbcc9a1
Bryana Ojopi <ojopibryana5@gmail.com>
2023-04-28 11:27:10 -0400

    configuracion de las variables de entorno

--- src/middlewares/validate-jwt.js
+++ src/middlewares/validate-jwt.js
@@ -4,8 +4,6 @@ import { Person } from '../models/Person.js';
 import { Role } from '../models/Role.js';
 import { verify } from 'jsonwebtoken';
 
-const jwtSecret = process.env.JWT_SECRET
-
 export const validateJWT = async (req = request, res = response, next) => {
     const { token } = req.cookies;
 
@@ -16,7 +14,7 @@ export const validateJWT = async (req = request, res = response, next) => {
     }
 
     try {
-        const { uid } = verify(token, jwtSecret);
+        const { uid } = verify(token, process.env.JWT_SECRET);
 
         const profile = await Profile.findOne({where: { id_profile: uid }, include: [Person, Role]});
 
