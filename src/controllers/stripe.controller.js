2e63641122847f5b02755a1aa82fae13b2208973
Bryana Ojopi <ojopibryana5@gmail.com>
2023-08-29 09:45:08 -0400

    cambios de stripe

--- src/controllers/stripe.controller.js
+++ src/controllers/stripe.controller.js
@@ -4,13 +4,44 @@ import { Provider } from '../models/Provider.js';
 
 const str = stripe('sk_test_51N3mL2KHtcU5N9YX9BCRudz7A2vkrUIuVAdjF1i5noVFR2mdoDva8Mu8wbWsMHwKhx2BQfL7FutZHlu4J2DKfreZ00E8ODpuvf');
 
-export const account = async (req, res = response) => {
+export const createAccount = async (req, res = response) => {
     const { idProvider } = req.params;
-    const { email } = req.body
+    const { 
+        email, 
+        city,
+        address, 
+        shortCountry, 
+        postalCode,
+        phone,
+        businessName
+    } = req.body
 
     try {
         const stripeAccount = await str.accounts.create({
-            type: 'standard',
+            type: 'express',
+            country: 'US',
+            email,
+            business_type: 'company',
+            company: {
+                address: {
+                    city: city,
+                    country: shortCountry,
+                    line1: address,
+                    postal_code: postalCode
+                },
+                phone: phone
+            },
+            business_profile: {
+                name: businessName,
+                support_address: {
+                    city: city,
+                    country: shortCountry,
+                    line1: address,
+                    postal_code: postalCode
+                },
+                support_email: email,
+                support_phone: phone
+            }
         })
 
         if(stripeAccount) {
@@ -20,20 +51,55 @@ export const account = async (req, res = response) => {
                 where: {id_provider: idProvider}
             });
 
-            const accountLink = await str.accountLinks.create({
-                account: stripeAccount.id,
-                refresh_url: 'http://localhost:3000/welcome/profile',
-                return_url: 'http://localhost:3000/welcome/profile',
-                type: 'account_onboarding'
-            })
+            // const accountLink = await str.accountLinks.create({
+            //     account: stripeAccount.id,
+            //     refresh_url: 'http://localhost:3000/welcome/profile',
+            //     return_url: 'http://localhost:3000/welcome/profile',
+            //     type: 'account_onboarding'
+            // })
             
             res.status(200).json({
                 msg: 'Stripe Account Created',
                 stripeAccount,
-                accountLink
+                // accountLink
             });
         }
 
+    } catch (error) {
+        return res.status(400).json({msg: error.message});
+    }
+}
+
+export const createPaymentMethod = async (req, res = response) => {
+    const { token } = req.body;
+    try {
+        const paymentMethod = await str.paymentMethods.create({
+            type: 'card',
+            card: {
+                token: token
+            },
+        })
+
+        res.status(200).json({
+            msg: 'Payment Method Created',
+            paymentMethod,
+        });
+
+    } catch (error) {
+        return res.status(400).json({msg: error.message});
+    }
+}
+
+export const paymentMethodList = async (req, res = response) => {
+    try {
+        const paymentMethods = await str.customers.listPaymentMethods(
+            'cus_OXBiAwFV9w1aKC',
+            {type: 'card'}
+        );
+
+        res.status(200).json({
+            paymentMethods
+        });
     } catch (error) {
         return res.status(400).json({msg: error.message});
     }
--- src/routes/stripe.routes.js
+++ src/routes/stripe.routes.js
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import { validateJWT, validateJWTExpired } from '../middlewares/validate-jwt.js';
 import { validateRol } from '../middlewares/validate-rol.js';
-import { account } from "../controllers/stripe.controller.js";
+import { createAccount, createPaymentMethod, paymentMethodList } from "../controllers/stripe.controller.js";
 
 const router = Router();
 
-router.post('/stripe-create/:idProvider', account);
+router.get('/list-payment-methods', paymentMethodList);
+
+router.post('/stripe-create/:idProvider', createAccount);
+
+router.post('/payment-method-create', createPaymentMethod);
 
 export default router;
